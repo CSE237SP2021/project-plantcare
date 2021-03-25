4e15b681b4d36a60dedcb38b881e12859a7ed6cc
David Papermaster <papermasterd@gmail.com>
2021-03-25 18:28:09 -0500

    Begin Plant Information Feature

---
+++ src/PlantInformation.java
@@ -0,0 +1,18 @@
+
+public class PlantInformation implements Feature {
+	
+	public PlantInformation() {
+		
+	}
+	
+	public String getLabel() {
+		return "Plant Information";
+	}
+	
+	public void run() {
+		System.out.println("Showing plant information...");
+		// TODO Auto-generated method stub
+
+	}
+
+}
