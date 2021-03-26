6261c330467dd6135865f97c55e649f318c4d62e
David Papermaster <papermasterd@gmail.com>
2021-03-25 19:38:54 -0500

    Basic plant info menu features

---
+++ src/plantInformation/SearchByCareLevel.java
@@ -0,0 +1,18 @@
+package plantInformation;
+
+import main.Feature;
+
+public class SearchByCareLevel implements Feature {
+
+	@Override
+	public String getLabel() {
+		return "Search by level of care";
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Searching by level of care...");
+	}
+
+}
---
+++ src/plantInformation/SearchByName.java
@@ -0,0 +1,18 @@
+package plantInformation;
+
+import main.Feature;
+
+public class SearchByName implements Feature {
+
+	@Override
+	public String getLabel() {
+		return "Search by plant name";
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Searching by plant name...");
+	}
+
+}
