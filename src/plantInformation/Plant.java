5af9ae81a64c4cb78ebd6ab90f0c82a33f22137f
杨笑海 <@DESKTOP-056CQ1A>
2021-03-29 14:03:01 +0800

    added some comment

--- src/plantInformation/Plant.java
+++ src/plantInformation/Plant.java
@@ -1,4 +1,8 @@
 package plantInformation;
+import main.Feature;
+import main.Menu;
+
+//This class define a special plant object. The plantSpecies might change to other type to better interact with other classes and help determined the features of the owned plant.
 
 public class Plant {
 	public String plantName;
@@ -6,15 +10,15 @@ public class Plant {
 	private int plantAge;
 	private int waterTrack;
 	private int waterPeriod;
+	private String owner;		
 	
-	
-	
-	public Plant(String name, String species, int age) {
+	public Plant(String name, String species, String owner, int age) {
 		plantName = name;
 		plantAge = age;
 		plantSpecies = species;
 		waterTrack = 0;
 		waterPeriod = findPeriod(species);
+		this.owner = owner;
 	}
 	
 	public Plant(String name, String species, int age, int water) {
@@ -23,8 +27,26 @@ public class Plant {
 		plantSpecies = species;
 		waterTrack = water;
 		waterPeriod = findPeriod(species);
+		this.owner = owner;
 	}
 	
+	
+	public String getLabel() {
+		return "Owned Plant";
+	}
+	
+	public void run() {
+		// LINK TO Plant information https://www.plantssparkjoy.com/common-house-plants/
+		
+		System.out.println("Showing plant information...");
+		
+		Menu menu = new Menu();
+		menu.addFeature(new ShowAllInfo());
+		menu.addFeature(new SearchByName());
+		menu.addFeature(new SearchByCareLevel());
+		
+		menu.makeSelectionLoop();
+	}
 	
 	public String getPlantName() {
 		return plantName;
@@ -52,17 +74,15 @@ public class Plant {
 		return waterPeriod - waterTrack;
 	}
 	
+	public String getOwner() {
+		return owner;
+	}
+	
+	//return desired water period based on the species
 	public int findPeriod(String species) {
 		return 0;
 	}
 	
-	public int findPhase(String species, int age) {
-		return 0;
-	}
-	
-	public int findAge(String species, int phase) {
-		return 0;
-	}
 	
 	public void setPlantName(String newName) {
 		plantName = newName;
@@ -77,7 +97,7 @@ public class Plant {
 	}
 	
 	
-	
+	//display information of the plant
 	public String toString() {
 		return plantName + ": " + plantAge + "months old " + plantSpecies;
 	}
