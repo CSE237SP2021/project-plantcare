c47116610c8a170e13f7c80cffbb3bbcd250d638
David Papermaster <papermasterd@gmail.com>
2021-03-31 15:19:07 -0500

    Working plant information features. Read information csv.

---
+++ src/main/plantInfo/PlantInformationTest.java
@@ -0,0 +1,49 @@
+package main.plantInfo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystems;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+class PlantInformationTest {
+
+	@Test
+	void addPlantTest() {
+		PlantInformation testPI = new PlantInformation();
+		Plant testPlant = new Plant("test");
+		testPI.addPlant(testPlant);
+		assertEquals(1,testPI.getNumPlants());
+	}
+	
+	
+	@Test
+	void readPlantInformationTest() {
+		//Compare number of lines in information csv to number of plants read into arraylist plants
+		
+		
+		String testCSVPath = "./src/main/plantInfo/plantInformation.csv";
+		File testCSV = new File(testCSVPath);
+		int plantCount = 0;
+		try {
+			Scanner testScanner = new Scanner(testCSV);
+			testScanner.useDelimiter("\n");
+			//skip label row
+			testScanner.next();
+			while(testScanner.hasNext()) {
+				testScanner.next();
+				plantCount++;
+			}
+			testScanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		PlantInformation testPI = new PlantInformation();
+		assertEquals(plantCount, testPI.getNumPlants());
+	}
+
+}
