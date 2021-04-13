123f31060a46c17ed020675f93d24b17b8236fd1
bridg <bridg@DESKTOP-A0FORI0.dhcp.wustl.edu>
2021-04-13 15:24:54 -0500

    created DeleteReminder

---
+++ src/main/reminders/DeleteReminder.java
@@ -0,0 +1,25 @@
+package main.reminders;
+
+import main.Feature;
+import main.Plant;
+import main.Menu;
+
+public class DeleteReminder implements Feature {
+
+	public DeleteReminder() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/reminders/WaterReminders.java
+++ src/main/reminders/WaterReminders.java
@@ -2,8 +2,6 @@ package main.reminders;
 import main.Feature;
 import main.Menu;
 import main.Plant;
-import main.plantInfo.InfoByCareLevel;
-import main.plantInfo.ShowAllInfo;
 
 import java.util.HashMap;
 import java.util.*;
@@ -16,7 +14,7 @@ public class WaterReminders implements Feature{
 		reminders = new HashMap<Plant, Reminder>();
 	}
 	
-	public void addReminder(HashMap<Plant, Reminder>) {
+	public void addReminder(HashMap<Plant, Reminder> reminders) {
 		System.out.print("What date will you start watering this plant? (Please use the format YYYY-MM-DD)");
 		Scanner sc = new Scanner(System.in);
 		String startDate = sc.nextLine();
@@ -43,10 +41,10 @@ public class WaterReminders implements Feature{
 
 	@Override
 	public void run() {
-		Menu menu = new Menu();
-		menu.addFeature(new AddReminder(reminders));
-		menu.addFeature();
+		Menu waterMenu = new Menu();
+		waterMenu.addFeature(new AddReminder(reminders));
+		waterMenu.addFeature(new DeleteReminder());
 		
-		menu.makeSelectionLoop();
+		waterMenu.makeSelectionLoop();
 	}
 }
