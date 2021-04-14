e6acea123809e9fc9b7328f2fe6ad73cb5e184e0
杨笑海 <@DESKTOP-056CQ1A>
2021-04-14 22:10:17 +0800

    draft deleteplanttest based on addplanttest

--- src/tests/TrackPlantsTest.java
+++ src/tests/TrackPlantsTest.java
@@ -81,5 +81,49 @@ class TrackPlantsTest {
 		assertEquals(beforePlantCount + 1, plantCount);
 		assertEquals(beforePlantCount + 1, testTracker.getNumPlants());
 	}
+	
+	@Test
+	void testDeletePlant() {
+		String testCSVPath = "./src/main/trackPlants/myPlants.csv";
+		File testCSV = new File(testCSVPath);
+		int beforePlantCount = 0;
+		try {
+			Scanner testScanner = new Scanner(testCSV);
+			testScanner.useDelimiter("\n");
+			//skip label row
+			testScanner.next();
+			while(testScanner.hasNext()) {
+				testScanner.next();
+				beforePlantCount++;
+			}
+			testScanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		TrackPlants testTracker = new TrackPlants();
+		testTracker.setPlantList(testTracker.readPlantList(testCSVPath));
+		Plant testPlant = testTracker.getPlant(2);
+		testTracker.deletePlant(2);
+		
+		File testCSVpost = new File(testCSVPath);
+		int plantCount = 0;
+		try {
+			Scanner testScanner = new Scanner(testCSVpost);
+			testScanner.useDelimiter("\n");
+			//skip label row
+			testScanner.next();
+			while(testScanner.hasNext()) {
+				testScanner.next();
+				plantCount++;
+			}
+			testScanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		assertEquals(beforePlantCount - 1, plantCount);
+		assertEquals(beforePlantCount - 1, testTracker.getNumPlants());
+	}
+
 
 }
