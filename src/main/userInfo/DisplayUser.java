3507a54a9b2cb27e64c09493b640505b52ff32e8
huhaoran <huhaoran@10.68.58.178>
2021-04-28 22:01:48 +0800

    further fixes

--- src/main/userInfo/DisplayUser.java
+++ src/main/userInfo/DisplayUser.java
@@ -6,9 +6,11 @@ import main.Feature;
 import main.Menu;
 
 public class DisplayUser implements Feature{
-	private Menu menu;
-	public DisplayUser(Menu menu) {
-		this.menu = menu;
+	private UserInfo uinfo;
+	
+	
+	public DisplayUser(UserInfo uinfo) {
+		this.uinfo = uinfo;
 	}
 	
 	// Get label to appear in menu
@@ -18,18 +20,28 @@ public class DisplayUser implements Feature{
 		
 	// Complete desired action
 	public void run() {
-		Scanner scanner = menu.getScanner();
+		
+		if(uinfo.getNumUsers()==0) {
+			System.out.println("No user exists");
+			return;
+		}
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Tell us the name of the user you want to view");
 		String nameInput = scanner.nextLine();
+		
 		//TODO: check if the name exist
-		if(nameInput == "") {
-			//TODO: loop find index through name;
-			System.out.println(menu.getMyUsers().getUser(0));
-			System.out.println(menu.getMyUsers().getCare(0));
-			System.out.println(menu.getMyUsers().getLight(0));
-			System.out.println(menu.getMyUsers().getLoc(0));
-		}else {
-			System.out.println("You don't have a user with this name.");
+
+		while(!uinfo.getAllUsers().contains(nameInput)) {
+			//TODO: loop find index through name;			
+			System.out.println("This username does not exist in the datatbase! Please try again!");
+			nameInput = scanner.nextLine();
 		}
+		
+		int index = uinfo.getAllUsers().indexOf(nameInput);
+		System.out.println(uinfo.getUser(index));
+		System.out.println(uinfo.getCare(index));
+		System.out.println(uinfo.getLight(index));
+		System.out.println(uinfo.getLoc(index));
+
 	}
 }
